e4cf5144853676aec75cb44683dcf547298d64cd
ParvezMia <parvezmia999@hotmail.com>
2025-04-05 22:51:59 +0600

    refactor: update namespace in LoginToken and PasswordlessLoginNotification
    Update the namespace from `ParvezMia\PasswordlessAuth` to `ParvezMia\LaravelPasswordlessAuth` to better reflect the package's context and improve consistency with Laravel naming conventions.

--- src/Models/LoginToken.php
+++ src/Models/LoginToken.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace ParvezMia\PasswordlessAuth\Models;
+namespace ParvezMia\LaravelPasswordlessAuth\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Str;
--- src/Notifications/PasswordlessLoginNotification.php
+++ src/Notifications/PasswordlessLoginNotification.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace ParvezMia\PasswordlessAuth\Notifications;
+namespace ParvezMia\LaravelPasswordlessAuth\Notifications;
 
 use Illuminate\Bus\Queueable;
 use Illuminate\Contracts\Queue\ShouldQueue;
