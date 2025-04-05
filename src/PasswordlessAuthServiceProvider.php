a63ff8d6a1ff8b7310b70f6640ba3d7a1adb1cab
ParvezMia <parvezmia999@hotmail.com>
2025-04-05 22:25:41 +0600

    refactor(PasswordlessAuthServiceProvider): simplify and reorganize service provider
    - Removed redundant publish configurations for email, controllers, and models
    - Reorganized the order of loading views, migrations, and routes for better readability
    - Simplified the migration publishing logic to publish all migrations at once

--- src/PasswordlessAuthServiceProvider.php
+++ src/PasswordlessAuthServiceProvider.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Jea\PasswordlessAuth;
+namespace ParvezMia\LaravelPasswordlessAuth;
 
 use Illuminate\Support\ServiceProvider;
 
@@ -11,14 +11,14 @@ class PasswordlessAuthServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        // Publish configuration
+        // Publish config
         $this->publishes([
             __DIR__.'/../config/passwordless-auth.php' => config_path('passwordless-auth.php'),
         ], 'config');
 
         // Publish migrations
         $this->publishes([
-            __DIR__.'/../database/migrations/create_login_tokens_table.php' => database_path('migrations/'.date('Y_m_d_His', time()).'_create_login_tokens_table.php'),
+            __DIR__.'/../database/migrations/' => database_path('migrations'),
         ], 'migrations');
 
         // Publish views
@@ -31,41 +31,14 @@ class PasswordlessAuthServiceProvider extends ServiceProvider
             __DIR__.'/../routes/passwordless-auth.php' => base_path('routes/passwordless-auth.php'),
         ], 'routes');
 
-        // Publish email-related files
-        $this->publishes([
-            __DIR__.'/../src/Notifications' => app_path('Notifications/Passwordless'),
-            __DIR__.'/../resources/views/emails' => resource_path('views/vendor/passwordless-auth/emails'),
-        ], 'email');
-
-        // Publish controllers
-        $this->publishes([
-            __DIR__.'/../src/Http/Controllers' => app_path('Http/Controllers/Passwordless'),
-        ], 'controllers');
-
-        // Publish models
-        $this->publishes([
-            __DIR__.'/../src/Models' => app_path('Models/Passwordless'),
-        ], 'models');
-
-        // Publish all components at once
-        $this->publishes([
-            __DIR__.'/../config/passwordless-auth.php' => config_path('passwordless-auth.php'),
-            __DIR__.'/../database/migrations/create_login_tokens_table.php' => database_path('migrations/'.date('Y_m_d_His', time()).'_create_login_tokens_table.php'),
-            __DIR__.'/../resources/views' => resource_path('views/vendor/passwordless-auth'),
-            __DIR__.'/../routes/passwordless-auth.php' => base_path('routes/passwordless-auth.php'),
-            __DIR__.'/../src/Notifications' => app_path('Notifications/Passwordless'),
-            __DIR__.'/../src/Http/Controllers' => app_path('Http/Controllers/Passwordless'),
-            __DIR__.'/../src/Models' => app_path('Models/Passwordless'),
-        ], 'all');
-
-        // Load routes
-        $this->loadRoutesFrom(__DIR__.'/../routes/web.php');
-
-        // Load views - ensure this is correctly loading the views
+        // Load views
         $this->loadViewsFrom(__DIR__.'/../resources/views', 'passwordless-auth');
 
         // Load migrations
         $this->loadMigrationsFrom(__DIR__.'/../database/migrations');
+
+        // Load routes
+        $this->loadRoutesFrom(__DIR__.'/../routes/web.php');
     }
 
     /**
@@ -74,6 +47,8 @@ class PasswordlessAuthServiceProvider extends ServiceProvider
     public function register()
     {
         // Merge config
-        $this->mergeConfigFrom(__DIR__.'/../config/passwordless-auth.php', 'passwordless-auth');
+        $this->mergeConfigFrom(
+            __DIR__.'/../config/passwordless-auth.php', 'passwordless-auth'
+        );
     }
 }
